9a8bd9deed4fbe98926d9ba076c8f19f765f901c
khobragade2001 <khobragade2001@gmail.com>
2024-08-24 00:44:55 +0530

    driver added

--- demo.py
+++ demo.py
@@ -1,11 +1,15 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 # making for 'requirements.txt' , following commond used
 # pip freeze > requirements.txt
 
-driver = webdriver.Chrome()
+opt = Options()
+opt.binary_location = "C:\\circle_ci\\chromedriver-win64\\chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path="C:\\circle_ci\\chromedriver-win64\\chromedriver.exe",options=opt)
 driver.maximize_window()
 driver.get("https://the-internet.herokuapp.com/dropdown")
 element = Select(driver.find_element(By.XPATH,"//select[@id='dropdown']"))
